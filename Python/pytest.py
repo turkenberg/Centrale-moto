f29a33107ba693aa2ebe0b598d7573ec4bdc5260
Antoine <antoine.bodin@gadz.org>
2019-03-24 16:59:11 +0100

    wandering arnds

--- Python/pytest.py
+++ Python/pytest.py
@@ -1,27 +1,5 @@
 #!/usr/bin/python
 
-from tkinter import *
+from tkinter import messagebox
  
-from tkinter.ttk import Progressbar
- 
-from tkinter import ttk
- 
-window = Tk()
- 
-window.title("Welcome to LikeGeeks app")
- 
-window.geometry('350x200')
- 
-style = ttk.Style()
- 
-style.theme_use('default')
- 
-style.configure("black.Horizontal.TProgressbar", background='black')
- 
-bar = Progressbar(window, length=200, style='black.Horizontal.TProgressbar')
- 
-bar['value'] = 70
- 
-bar.grid(column=0, row=0)
- 
-window.mainloop()
+messagebox.showinfo('Message title','Message content')
