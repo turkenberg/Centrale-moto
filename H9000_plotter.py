60ec1f959c2cc0660d1ca4fba3fabedda271d39c
Antoine <antoine.bodin@gadz.org>
2019-03-24 22:37:37 +0100

    liuher

--- H9000_plotter.py
+++ H9000_plotter.py
@@ -20,26 +20,27 @@ fig, ax = plt.subplots()
 line, = ax.plot(y_var)
 
 while True:
-        c = ser.read() # attempt to read a character from Serial
+    c = ser.read() # attempt to read a character from Serial
 
-        #was anything read?
-        if len(c) == 0:         # nothing read
-            break  
+    #was anything read?
+    if len(c) == 0:         # nothing read
+        break
+    
+    if c == '\n':           # received new line ?
+        isOpen = 1         # re-open buffer
+        break
 
-        if isOpen == 1:
-            if c == '\t': # PRINT instruction AND opened ; open resets at NL
-                print(serBuffer + '.') 
-                y_var = np.append(y_var, float(serBuffer))
-                y_var = y_var[1:plot_window+1]
-                line.set_ydata(y_var)
-                ax.relim()
-                ax.autoscale_view()
-                fig.canvas.draw()
-                fig.canvas.flush_events()
-                serBuffer = '' #empty buffer (to be filled until newline)
-                isOpen = 0     # close buffer until newline
-            else:
-                serBuffer += c  # add char to buffer if open
-
-        if c == '\n':           # received new line ?
-            isOpen = 1         # re-open buffer
+    if isOpen == 1:
+        if c == '\t':           # PRINT instruction ; open resets at NL
+            print(serBuffer + '.') 
+            y_var = np.append(y_var, float(serBuffer))
+            y_var = y_var[1:plot_window+1]
+            line.set_ydata(y_var)
+            ax.relim()
+            ax.autoscale_view()
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+            serBuffer = '' #empty buffer (to be filled until newline)
+            isOpen = 0     # close buffer until newline
+        else:
+            serBuffer += c  # add char to buffer if open
