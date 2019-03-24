087e0b59589f8268a4beb9749426afddf1a23dee
Antoine <antoine.bodin@gadz.org>
2019-03-24 22:29:18 +0100

    again?

--- H9000_plotter.py
+++ H9000_plotter.py
@@ -19,36 +19,30 @@ plt.ion()
 fig, ax = plt.subplots()
 line, = ax.plot(y_var)
 
-def readSerial():
-    while True:
-            c = ser.read() # attempt to read a character from Serial
+while True:
+        c = ser.read() # attempt to read a character from Serial
 
-            #was anything read?
-            if len(c) == 0:         # nothing read
-                break  
+        #was anything read?
+        if len(c) == 0:         # nothing read
+            break  
 
-            if isOpen == 1:
-                if c == '\t': # PRINT instruction AND opened ; open resets at NL
-                    print(serBuffer + '.')
-                    y_float = float(serBuffer)
-                    y_var = np.append(y_var, y_float)
-                    y_var = y_var[1:plot_window+1]
-                    line.set_ydata(y_var)
-                    ax.relim()
-                    ax.autoscale_view()
-                    fig.canvas.draw()
-                    fig.canvas.flush_events()
-                    serBuffer = "" #empty buffer (to be filled until newline)
-                    isOpen == 0     # close buffer until newline
-                else:
-                    serBuffer += c  # add char to buffer if open
+        if isOpen == 1:
+            if c == '\t': # PRINT instruction AND opened ; open resets at NL
+                print(serBuffer + '.')
+                y_float = float(serBuffer)
+                y_var = np.append(y_var, y_float)
+                y_var = y_var[1:plot_window+1]
+                line.set_ydata(y_var)
+                ax.relim()
+                ax.autoscale_view()
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+                serBuffer = "" #empty buffer (to be filled until newline)
+                isOpen == 0     # close buffer until newline
+                break
+            else:
+                serBuffer += c  # add char to buffer if open
 
-            if c == '\n':           # received new line ?
-                isOpen == 1         # re-open buffer
-                serBuffer = ""
-    root.after(10, readSerial)
-
-# after initializing serial, an arduino may need a bit of time to reset
-root.after(100, readSerial)
-
-app.mainloop()
+        if c == '\n':           # received new line ?
+            isOpen == 1         # re-open buffer
+            serBuffer = ""
